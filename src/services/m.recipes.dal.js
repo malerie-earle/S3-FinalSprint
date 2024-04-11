c7a10a9aee7c64ddee6a667d4d866b531ccf8c00
Malerie <malerie.earle@keyin.com>
2024-04-11 01:25:56 -0230

    set up mongo connection, recipe dal, router and allRecipe page, and pagination

--- src/services/m.recipes.dal.js
+++ src/services/m.recipes.dal.js
@@ -3,16 +3,27 @@ const ObjectId = mongodb.ObjectId;
 const dal = require("./m.auth_db.js");
 const logger = require("../logEvents.js");
 
-async function getAllRecipes() {
-  logger.info('mongo.dal.getRecipes()');
+async function getAllRecipes(page = 1, pageSize = 50) {
   try {
     await dal.connect();
     logger.info('Connected to MongoDB');
+
+    // Parse page and pageSize to integers
+    page = !isNaN(Number(page)) ? Number(page) : 1;
+    pageSize = !isNaN(Number(pageSize)) ? Number(pageSize) : 50;
+
     const recipes = dal.db.collection('Recipes');
-    const result = await recipes.find().toArray();
-    logger.info(result);
-    return result;
-  } catch(error) {
+
+    // Calculate skip value based on page and pageSize
+    const skip = (page - 1) * pageSize;
+
+    // Find recipes with pagination
+    logger.info(page, pageSize, skip);
+    const theRecipes = await recipes.find().skip(skip).limit(pageSize).toArray();
+
+    // logger.info(result);
+    return { page, pageSize, theRecipes };
+  } catch (error) {
     logger.error('Error occurred while fetching data from MongoDB:', error);
     throw error;
   } finally {
@@ -20,6 +31,12 @@ async function getAllRecipes() {
     dal.close();
   }
 }
+
+module.exports = {
+  getAllRecipes
+};
+
+
 
 async function getRecipeById(_id) {
   logger.info('mongo.dal.getRecipeById()');
